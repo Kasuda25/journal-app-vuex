6854faa6af76dca73e3e7fa7ee7840bfe6fbbcb6
Kasuda25 <119356855+Kasuda25@users.noreply.github.com>
2023-07-11 14:03:57 -0500

    first commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+  >
+    <div class="entry-title d-flex">
+      <span class="text-success fs-5 fw-bold">10</span>
+      <span class="mx-1 fs-5">Julio</span>
+      <span class="mx-2 fw-light">2023, lunes</span>
+    </div>
+    <div class="entry-description">
+      Eu magna qui est tempor amet do reprehenderit Lorem. Anim sint consectetur
+      magna enim commodo reprehenderit et et labore deserunt nostrud. Labore
+      exercitation eu ullamco veniam.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+  border-bottom: 1px solid #2c3e50;
+  transition: 0.2s all ease-in;
+
+  &:hover {
+    background-color: lighten($color: grey, $amount: 45);
+  }
+
+  .entry-description {
+    font-size: 12px;
+  }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            picture: null
+        },
+    ]
+})
