ccca4ad2dfda9967582c7a51b4af1b3b037e516e
Kasuda25 <119356855+Kasuda25@users.noreply.github.com>
2023-07-12 21:41:22 -0500

    update

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,21 +2,21 @@ export default () => ({
     isLoading: true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
-            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            text: 'Eu eiusmod duis amet sit proident excepteur do amet in proident velit. Sint cillum incididunt nostrud quis Lorem amet veniam culpa non. Qui cupidatat id enim nulla cupidatat tempor duis tempor quis minim eu. Velit ullamco aute nulla ea sint eiusmod laborum excepteur culpa sunt laborum. Deserunt sit cillum dolor minim proident commodo duis adipisicing elit irure voluptate ut adipisicing eu. Quis esse nostrud nisi nulla Lorem in dolore cupidatat ipsum anim qui.',
             picture: null
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
-            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            text: 'Incididunt incididunt fugiat pariatur eiusmod irure qui fugiat. Amet cupidatat amet culpa excepteur reprehenderit id laboris id ad duis fugiat. Ad irure aute cupidatat ea est in fugiat. Magna ex id enim amet veniam est quis pariatur sit. Nisi ea aliquip laborum ullamco ut. Irure ipsum ad irure deserunt est quis id elit adipisicing ut adipisicing cupidatat aliqua.',
             picture: null
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
-            text: 'Irure occaecat esse ipsum ipsum aliqua in aliquip sint pariatur cupidatat ut tempor pariatur tempor. Non irure dolor adipisicing cillum magna adipisicing tempor velit officia. Aliqua officia quis ipsum Lorem. Tempor tempor anim deserunt pariatur cupidatat incididunt consequat qui incididunt cillum aliquip voluptate anim deserunt. Voluptate est in enim quis. Consectetur veniam exercitation pariatur exercitation exercitation fugiat anim. Deserunt adipisicing consequat laboris laboris occaecat.',
+            text: 'Nostrud consectetur nisi deserunt magna do eiusmod consectetur commodo commodo laboris ut. Cupidatat dolore minim mollit consequat minim fugiat exercitation deserunt voluptate veniam ullamco dolore. Fugiat dolore occaecat nulla nisi. Do id culpa dolore velit in aliqua laboris ad et labore amet veniam exercitation cupidatat. Anim exercitation ex aliquip id amet.',
             picture: null
         },
     ]
